cf05681cbcc31e10cc97d63143f21852a888ee49
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-09-11 20:49:12 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
     if @song.save
-      redirect_to song_path(@song)
+      redirect_to @song
     else
       render :new
     end
